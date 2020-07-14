85417e232a868742da1f07e04f6fc95cb41f5934
Liana Meyer <liana_p@icloud.com>
2020-07-14 12:16:40 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+// Smooth Scrolling
+$('.nav-buttons a').on('click', function (event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      800,
+      function () {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
+
+// Smooth Scrolling
+$('.nav-item a').on('click', function (event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      800,
+      function () {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
+
+var menu = document.getElementById('menu');
+var rm = document.querySelector('.responsive-menu');
+menu.onclick = function () {
+  rm.classList.toggle('responsive-menu-show');
+};
