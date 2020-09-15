68913f8aec5a81d3d1c489400b221b515dc53613
Liana Meyer <liana_p@icloud.com>
2020-09-15 14:07:31 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -1,43 +1,7 @@
-// Smooth Scrolling
-$('.nav-buttons a').on('click', function (event) {
-  if (this.hash !== '') {
-    event.preventDefault();
+const ham = document.querySelector('.hamburger');
+const hamm = document.querySelector('.ham-m');
 
-    const hash = this.hash;
-
-    $('html, body').animate(
-      {
-        scrollTop: $(hash).offset().top,
-      },
-      800,
-      function () {
-        window.location.hash = hash;
-      }
-    );
-  }
-});
-
-// Smooth Scrolling
-$('.nav-item a').on('click', function (event) {
-  if (this.hash !== '') {
-    event.preventDefault();
-
-    const hash = this.hash;
-
-    $('html, body').animate(
-      {
-        scrollTop: $(hash).offset().top,
-      },
-      800,
-      function () {
-        window.location.hash = hash;
-      }
-    );
-  }
-});
-
-var menu = document.getElementById('menu');
-var rm = document.querySelector('.responsive-menu');
-menu.onclick = function () {
-  rm.classList.toggle('responsive-menu-show');
+ham.onclick = function () {
+  ham.classList.toggle('ham-c');
+  hamm.classList.toggle('show');
 };
