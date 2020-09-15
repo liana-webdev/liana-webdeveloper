27acff885598a28040ecb00b9666f48041fd8488
Liana Meyer <liana_p@icloud.com>
2020-09-15 15:15:41 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 const ham = document.querySelector('.hamburger');
 const hamm = document.querySelector('.ham-m');
-const navb = document.querySelector('.nav-b');
+const nav1 = document.querySelector('.nav-1');
+const nav2 = document.querySelector('.nav-2');
+const nav3 = document.querySelector('.nav-3');
+const nav4 = document.querySelector('.nav-4');
 
 ham.onclick = function () {
   ham.classList.toggle('ham-c');
   hamm.classList.toggle('show');
 };
 
-navb.onclick = function () {
+nav1.onclick = function () {
   ham.classList.toggle('ham-c');
   hamm.classList.toggle('show');
+  console.log('clicked');
+};
+nav2.onclick = function () {
+  ham.classList.toggle('ham-c');
+  hamm.classList.toggle('show');
+  console.log('clicked');
+};
+nav3.onclick = function () {
+  ham.classList.toggle('ham-c');
+  hamm.classList.toggle('show');
+  console.log('clicked');
+};
+nav4.onclick = function () {
+  ham.classList.toggle('ham-c');
+  hamm.classList.toggle('show');
+  console.log('clicked');
 };
