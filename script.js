6cd3c9284d1a4deff43b1c89adb9bfe36a021256
Liana Meyer <liana_p@icloud.com>
2020-09-15 15:09:04 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const ham = document.querySelector('.hamburger');
 const hamm = document.querySelector('.ham-m');
+const navb = document.querySelector('.nav-b');
 
 ham.onclick = function () {
+  ham.classList.toggle('ham-c');
+  hamm.classList.toggle('show');
+};
+
+navb.onclick = function () {
   ham.classList.toggle('ham-c');
   hamm.classList.toggle('show');
 };
